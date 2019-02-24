e27c26509f01064e252d7afac88ee0d4f94e789b
Roham <rzninvo@gmal.com>
2019-02-24 17:36:30 +0330

    Creating Methods for class student

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,31 @@
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String studentID;
+    private float grade;
+    public void setFirstName(String firstName){}
+    public void setLastName(String lastName){}
+    public void setStudentID(String ID){}
+    public Student(String firstName, String lastName, String studentID){}
+    public void setGrade(float grade){}
+    public float getGrade(){
+        return 0;
+    }
+    public String getFirstName(){
+        return "";
+    }
+    public String getLastName(){
+        return "";
+    }
+    public String getStudentID()
+    {
+        return "";
+    }
+    public String getInfo(){
+        return "";
+    }
+
 }
+
+
+
